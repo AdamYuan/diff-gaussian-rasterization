cba6e07bca93413f080c91b7749f2d61f3c34552
AdamYuan <y13916619121@126.com>
2025-04-09 14:52:08 +0800

    Revert "Try https://github.com/interestingLSY/diff-gaussian-rasterization/tree/intlsy but it's somehow slower"
    This reverts commit df294697f092cfc5b9eecf9add2cb5d46b319764.

--- cuda_rasterizer/rasterizer_impl.cu
+++ cuda_rasterizer/rasterizer_impl.cu
@@ -399,7 +399,6 @@ void CudaRasterizer::Rasterizer::backward(
 		color_ptr,
 		imgState.accum_alpha,
 		imgState.n_contrib,
-		geomState.tiles_touched,
 		dL_dpix,
 		(float3*)dL_dmean2D,
 		(float4*)dL_dconic,
