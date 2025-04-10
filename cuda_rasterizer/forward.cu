c8a8f61a7b0d096b39a597dd98181eadec026ac7
AdamYuan <y13916619121@126.com>
2025-04-10 22:45:11 +0800

    Small changes of diff-gaussian-rasterization to match the result of VkGSRaster

--- cuda_rasterizer/forward.cu
+++ cuda_rasterizer/forward.cu
@@ -344,7 +344,7 @@ renderCUDA(
 			if (alpha < 1.0f / 255.0f)
 				continue;
 			float test_T = T * (1 - alpha);
-			if (test_T < 0.0001f)
+			if (T < 0.0001f)
 			{
 				done = true;
 				continue;
